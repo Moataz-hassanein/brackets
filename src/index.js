eb43f0e95b288e5f7402f784de1ea9ad0435c3d6
Moataz-hassanein <moataz_hassaneen@hotmail.com>
2020-03-23 00:46:47 +0100

    adding

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var arr = [];
+  for (var i  = 0; i < str.length; i++ ){
+    if ( str[i] == "(" || str[i] == "{" || str[i] == "[" ) {
+      arr.push(str[i]);
+    }else if ( str[i] == ")" &&  arr[arr.length-1] == "("  ) {
+        arr.pop();
+    } else if ( str[i] == "}" &&  arr[arr.length-1] == "{"  ) {
+      arr.pop();
+    }else if  ( str[i] == "]" &&  arr[arr.length-1] == "["  ) {
+      arr.pop();
+      
+    } else if ( str[i] == "|" && arr[arr.length-1] == "|") {  
+        arr.pop();
+    }else if ( str[i] == "|" ) {
+      arr.push(str[i]);
+    } else {
+      return false;
+    }
+  }
+  if (arr.length == 0) {
+    return true;
+    
+  } else {
+    return false;
+  }
 }
